93213277713b9243e69cba8f9b9493198ecfb0c6
Nico <nicoconstantin11@gmail.com>
2023-04-30 23:21:46 -0300

    initial config and dependencies

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+}
---
+++ src/pages/_app.js
@@ -0,0 +1,8 @@
+import '@/styles/globals.css'
+import { appWithTranslation } from 'next-i18next'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default appWithTranslation(App)
